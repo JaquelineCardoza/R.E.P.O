f66752234008f462b9f5bc6c7f337485737aeaf9
jaqueline <cm22005@itcha.edu.sv>
2025-05-06 08:30:57 -0600

    ejercicio2

--- Tema1/2.cpp
+++ Tema1/2.cpp
@@ -11,6 +11,7 @@ int main()
     int num, resul;
     cout << "Please enter a number \n";
     cin >> num;
+    
     // variable para verificar si es par o impar, el resul tiene quedar 0 si es par y 1 si es impar
     resul = num % 2;
     if (resul == 0)
