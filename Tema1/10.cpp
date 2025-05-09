ea849903f458f422ff3035bea514f3d9e398fb1f
jaqueline <cm22005@itcha.edu.sv>
2025-05-08 21:41:13 -0600

    ejercicio10

--- Tema1/10.cpp
+++ Tema1/10.cpp
@@ -5,6 +5,27 @@ using namespace std;
 
 int main()
 {
-    
+    // variable
+    int peso;
+    cout << "Ingresa el peso del paquete en kg: \n";
+    cin >> peso;
+    // comparacion
+    if (peso > 0 and peso <= 5)
+    {
+        cout << "La tarifa es de $ 5.00 \n";
+    }
+    else if (peso > 5 && peso < 10)
+    {
+        cout << "La tarifa es de $ 10.00 \n";
+    }
+    else if (peso >= 10)
+    {
+        cout << "La tarifa es de $ 15.00 \n";
+    }
+    else
+    {
+        cout << "Los datos ingresados no son validos \n";
+    }
+
     return 0;
 }
