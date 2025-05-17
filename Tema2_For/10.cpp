79a0cc8cf7b138a83db80625cd5c7d6d5edaa959
JaquelineCardoza <00252125@uca.edu.sv>
2025-05-16 20:30:45 -0600

    ejercicio for de serie Fibonacci 🎉, ya con formato

--- Tema2_For/10.cpp
+++ Tema2_For/10.cpp
@@ -19,10 +19,10 @@ int main()
         cout << a; // imprime el número actual de la serie de Fibonacci y se verifica si i es menor al numero ingresado menos 1
         if (i < n - 1)
         {
-            cout << ", "; //Imprime la coma y un espacio después del número, por ejemplo: 0, 1, 1, ...
-            int next = a + b;//Calcula el siguiente número en la serie de Fibonacci sumando los dos anteriores 
-            a = b; // Mueve b a a, porque en la siguiente iteración a debe tomar el valor que tenía b.
-            b = next;// Asigna a b el nuevo número que calculamos (next), preparándose para la siguiente suma.
+            cout << ", ";     // Imprime la coma y un espacio después del número, por ejemplo: 0, 1, 1, ...
+            int next = a + b; // Calcula el siguiente número en la serie de Fibonacci sumando los dos anteriores
+            a = b;            // Mueve b a a, porque en la siguiente iteración a debe tomar el valor que tenía b.
+            b = next;         // Asigna a b el nuevo número que calculamos (next), preparándose para la siguiente suma.
         }
     }
     cout << endl;
