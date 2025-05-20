2ad58052aec592bfd051d986ca3c792ec6330d22
Jaqueline Nicole Cardoza Maldonado <00252125@uca.edu.sv>
2025-05-19 19:23:12 -0600

    update

---
+++ Tema4_Switch/9.cpp
@@ -0,0 +1,21 @@
+/*Días en un Mes
+Pide el número de un mes y muestra cuántos días tiene.*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // variables 
+    int mes;
+
+    cout << "Ingresa un números de 1 al 12 \n";
+    cin>> mes
+    if(mes == 1 || mes==3  ){
+
+    }
+
+    
+    return 0;
+}
