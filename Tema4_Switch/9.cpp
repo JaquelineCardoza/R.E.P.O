778a1a7bac0208e082f233fa59ddb86b04c4e371
JaquelineCardoza <00252125@uca.edu.sv>
2025-05-19 19:35:24 -0600

    📆 ejercicio Switch 9 (Dias en un mes) 📆

--- Tema4_Switch/9.cpp
+++ Tema4_Switch/9.cpp
@@ -1,21 +1,49 @@
 /*Días en un Mes
 Pide el número de un mes y muestra cuántos días tiene.*/
 
-
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
-    // variables 
+    // variables
     int mes;
+    int dia;
 
     cout << "Ingresa un números de 1 al 12 \n";
-    cin>> mes
-    if(mes == 1 || mes==3  ){
-
+    cin >> mes;
+    if (mes >= 1 && mes <= 12)
+    {
+        if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12)
+        {
+            dia = 1;
+        }
+        else if (mes == 2)
+        {
+            dia = 2;
+        }
+        else
+        {
+            dia = 1;
+        }
+        switch (dia)
+        {
+        case 1:
+            cout << "El mes ingresado tiene 31 dias";
+            break;
+        case 2:
+            cout << "El mes ingresado tiene 28 dias";
+            break;
+        default:
+            cout << "El mes ingresado tiene 30 dias";
+            break;
+        }
+    }
+    else
+    {
+        cout << "los datos ingresados no son validos";
     }
 
-    
     return 0;
 }
