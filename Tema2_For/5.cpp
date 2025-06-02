326ce6af4c7d68dfffd15d97f92e11caf6b0ffe3
JaquelineCardoza <00252125@uca.edu.sv>
2025-06-02 15:02:41 -0600

    Cuenta Regresiva 🎉

---
+++ Tema2_For/5.cpp
@@ -0,0 +1,15 @@
+/*Cuenta Regresiva
+Imprime una cuenta regresiva desde 10 hasta 0.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 10; i >= 0; i--)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
