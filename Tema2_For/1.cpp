7c45c47985612dc7066a495c30dd07d006c120ff
JaquelineCardoza <00252125@uca.edu.sv>
2025-06-02 14:23:39 -0600

    Contador del 1 al 10

---
+++ Tema2_For/1.cpp
@@ -0,0 +1,17 @@
+/*Contador del 1 al 10
+Escribe un programa que imprima los números del 1 al 10 utilizando un ciclo for.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 10;
+    for (int i = 1; i <= num; i++)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
