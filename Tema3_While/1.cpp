1c5122831f6330ac1384645f5780935eb059f950
JaquelineCardoza <00252125@uca.edu.sv>
2025-06-03 16:19:40 -0600

    ⏩ Contador del 1 al 10 ⏩

---
+++ Tema3_While/1.cpp
@@ -0,0 +1,16 @@
+/*Contador del 1 al 10
+Utilizando un ciclo while, imprime los números del 1 al 10.*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 1;
+    while (n <= 10 )
+    {
+        cout<< n << endl;
+        n++;
+    }
+
+    return 0;
+}
